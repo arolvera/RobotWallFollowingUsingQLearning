304c1c6dabacc23fd696e9b36a7c0e1d58f47ffa
Anthony Olvera <olvera.anthony12@gmail.com>
2021-03-10 11:59:16 -0700

    getting scan data

--- catkin_ws/src/follow_wall/scripts/follow_wall.py
+++ catkin_ws/src/follow_wall/scripts/follow_wall.py
@@ -1,35 +1,47 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-from nav_msgs import Odometry
-from nav_msgs import Path
-from sensor_msgs import PointCloud2
-from geometry_msgs.msg import Pose2D 
+#from nav_msgs import Odometry
+from sensor_msgs.msg import LaserScan
+#from nav_msgs import Path
+#from sensor_msgs import PointCloud2
+#from geometry_msgs.msg import Pose2D 
 
-def path_callback(data):
-    # Implement call to q learning 
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+# def path_callback(data):
+#     # Implement call to q learning 
+#     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
-def odom_callback(data):
-    # Implement call to q learning 
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+# def odom_callback(data):
+#     # Implement call to q learning 
+#     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
-def point_callback(data):
+# def point_callback(data):
+#     # Implement call to q learning 
+#     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+
+def scan_callback(data):
+    print(data.ranges)
     # Implement call to q learning 
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     
-def read_senors():
+    
+    
+def read_sensors():
+    
 
+        
+        
     rospy.init_node('read_sensors', anonymous=True)
 
-    rospy.Subscriber("/triton/path", Path, path_callback)
-    rospy.Subscriber("/triton/odom", Odometry, odom_callback)
-    rospy.Subscriber("/camera/depth/points", PointCloud2, point_callback)
+    rospy.Subscriber("/scan", LaserScan, scan_callback)
+    #rospy.Subscriber("/triton/path", Path, path_callback)
+    #rospy.Subscriber("/triton/odom", Odometry, odom_callback)
+    #rospy.Subscriber("/camera/depth/points", PointCloud2, point_callback)
     
     rospy.spin()
 
 def robot_control():
-    pub = rospy.Publisher('/triton/vel_cmd geometry_msgs/Pose2D', Pose2D, queue_size=10)
+    pub = rospy.Publisher('/triton_lidar/vel_cmd geometry_msgs/Pose2D', Pose2D, queue_size=10)
     rospy.init_node('robot_control', anonymous=True)
     rate = rospy.Rate(20) # 20hz
     while not rospy.is_shutdown():
@@ -40,7 +52,7 @@ def robot_control():
         rate.sleep()
 
 def main():
-    pass
+    read_sensors()
 
 if __name__ == '__main__':
     try:
--- stingray_setup.bash
+++ stingray_setup.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export GAZEBO_RESOURCE_PATH=~/Stingray-Simulation/catkin_ws/src/stingray_sim
-export GAZEBO_MODEL_PATH=~/Stingray-Simulation/catkin_ws/src/stingray_sim/models
-export GAZEBO_PLUGIN_PATH=~/Stingray-Simulation/catkin_ws/src/stingray_sim/plugins/build
+export GAZEBO_RESOURCE_PATH=~/coursework/CSCI573/CSCI573_Project2/catkin_ws/src/stingray_sim
+export GAZEBO_MODEL_PATH=~/coursework/CSCI573/CSCI573_Project2/catkin_ws/src/stingray_sim/models
+export GAZEBO_PLUGIN_PATH=~/coursework/CSCI573/CSCI573_Project2/catkin_ws/src/stingray_sim/plugins/build
 
