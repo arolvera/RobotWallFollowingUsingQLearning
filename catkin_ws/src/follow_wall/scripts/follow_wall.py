ffbea1d5f931e81f92ee4b5b1e0b774ebd7de421
Anthony Olvera <olvera.anthony12@gmail.com>
2021-03-10 18:37:12 -0700

    Robot takes random actions

--- catkin_ws/src/follow_wall/scripts/follow_wall.py
+++ catkin_ws/src/follow_wall/scripts/follow_wall.py
@@ -1,58 +1,65 @@
 #!/usr/bin/env python
 import rospy
+import math
+import random
+import numpy as np
 from std_msgs.msg import String
-#from nav_msgs import Odometry
 from sensor_msgs.msg import LaserScan
-#from nav_msgs import Path
-#from sensor_msgs import PointCloud2
-#from geometry_msgs.msg import Pose2D 
+from geometry_msgs.msg import Pose2D 
 
-# def path_callback(data):
-#     # Implement call to q learning 
-#     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+# Initialize qtable of all zeros df of shape (actions, states)
+states = ["to close", "to far", "correct_distance"]
+actions = ["forward", "left", "right"]
+#Q_table = pd.DataFrame(0, index=np.arange(len(states)), columns=actions)
+#df.index = states
 
-# def odom_callback(data):
-#     # Implement call to q learning 
-#     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+# Define discount factor
+discount_factor = 0.9
 
-# def point_callback(data):
-#     # Implement call to q learning 
-#     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+msg = Pose2D() # Initialize a Pose2D message
+# Publish Pose2D messages on vel_cmd topic to triton_lidar node 
+pub = rospy.Publisher('/triton_lidar/vel_cmd', Pose2D, queue_size=10)
 
+def Q(state, action):
+    pass
 
-def scan_callback(data):
-    print(data.ranges)
-    # Implement call to q learning 
+def scan_callback(data): # Get state from here 
+    rospy.loginfo("data: %s", data)
+
+    # rosservice call /gazebo/get_model_state '{model_name: triton_lidar}'
+
+def execute_random_action(duration):
+    action = random.randint(0,2)
+    if action == 0:       # Forward case
+        msg.x = 0.3
+    elif action == 1:     # Left case
+        msg.x = 0.3
+        msg.theta = math.pi/4
+    else:                     # Right case
+        msg.x = 0.3
+        msg.theta = -math.pi/4
+    current_timestep = rospy.Time.now()
+    future_timestep = current_timestep + duration
+    while rospy.Time.now() < future_timestep:
+        pub.publish(msg) # Take action for one timestep
+    msg.x = 0
+    msg.theta = 0
+    pub.publish(msg)
+    return action
     
-    
-    
-def read_sensors():
-    
-
-        
-        
-    rospy.init_node('read_sensors', anonymous=True)
-
-    rospy.Subscriber("/scan", LaserScan, scan_callback)
-    #rospy.Subscriber("/triton/path", Path, path_callback)
-    #rospy.Subscriber("/triton/odom", Odometry, odom_callback)
-    #rospy.Subscriber("/camera/depth/points", PointCloud2, point_callback)
-    
-    rospy.spin()
-
-def robot_control():
-    pub = rospy.Publisher('/triton_lidar/vel_cmd geometry_msgs/Pose2D', Pose2D, queue_size=10)
-    rospy.init_node('robot_control', anonymous=True)
-    rate = rospy.Rate(20) # 20hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        # Control code goes here
-        # pub.publish(hello_str)
-        rate.sleep()
-
 def main():
-    read_sensors()
+    rospy.init_node('execute_action', anonymous=True)
+    rate = rospy.Rate(20) # 20hz
+    rospy.Subscriber("/scan", LaserScan, scan_callback)
+    timestep_duration = rospy.Duration(1) # One second
+    while not rospy.is_shutdown():
+        action = execute_random_action(timestep_duration) # Take action
+        # Get state
+        reward = Q(state, action)
+
+        #state = rosservice call /gazebo/get_model_state '{model_name: triton_lidar}'
+    #read_sensors()
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
--- stingray_setup.bash
+++ stingray_setup.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export GAZEBO_RESOURCE_PATH=~/coursework/CSCI573/CSCI573_Project2/catkin_ws/src/stingray_sim
-export GAZEBO_MODEL_PATH=~/coursework/CSCI573/CSCI573_Project2/catkin_ws/src/stingray_sim/models
-export GAZEBO_PLUGIN_PATH=~/coursework/CSCI573/CSCI573_Project2/catkin_ws/src/stingray_sim/plugins/build
+export GAZEBO_RESOURCE_PATH=~/Mines/CSCI573/project2/catkin_ws/src/stingray_sim
+export GAZEBO_MODEL_PATH=~/Mines/CSCI573/project2/catkin_ws/src/stingray_sim/models
+export GAZEBO_PLUGIN_PATH=~/Mines/CSCI573/project2/catkin_ws/src/stingray_sim/plugins/build
 
