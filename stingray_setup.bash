ce214b4dd526abeadd45a64aea3f3afb5724a966
aolvera <aolvera@exoterra.com>
2023-12-15 20:33:42 +0000

    Fix path

--- stingray_setup.bash
+++ stingray_setup.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-export GAZEBO_RESOURCE_PATH=~/Mines/CSCI573/project2/catkin_ws/src/stingray_sim
-export GAZEBO_MODEL_PATH=~/Mines/CSCI573/project2/catkin_ws/src/stingray_sim/models
-export GAZEBO_PLUGIN_PATH=~/Mines/CSCI573/project2/catkin_ws/src/stingray_sim/plugins/build
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+export GAZEBO_RESOURCE_PATH=${SCRIPT_DIR}/catkin_ws/src/stingray_sim
+export GAZEBO_MODEL_PATH=${SCRIPT_DIR}/catkin_ws/src/stingray_sim/models
+export GAZEBO_PLUGIN_PATH=${SCRIPT_DIR}/catkin_ws/src/stingray_sim/plugins/build
